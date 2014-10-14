ef38439b8fe433c4f776cd18f422c4e980643b04
Urs Liska <git@ursliska.de>
2014-10-14 11:23:44 +0200

    project-status.py: Initial stub

---
+++ python/project-status.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+# This file is part of the Gruppen project
+# https://git.ursliska.de/openlilylib/gruppen
+#
+# Copyright (c) 2014 by Urs Liska and others
+# (as documented by the Git history)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,   
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# See http://www.gnu.org/licenses/ for more information.
+
+"""
+Generate status data of a Gruppen compliant repository.
+By default create a JSON file with timestamp, optionally an HTML file instead.
+Optionally remove JSON files with a timestamp of the same day that are found
+in the target directory (to have always at most *one* file per day).
+"""
+
+import sys
+import os
+
+import init
+import commandline
+import project
+
+def main():
+    global project
+    args = commandline.parse()
+    # try to open a project
+    try:
+        project = project.Project(args)
+    except AssertionError, e:
+        print '\n', e, '\n'
+        sys.exit(1)
+ 
+    
+    print project.status.time_stamp()
+
+# ####################################
+# Finally launch the program
+if __name__ == "__main__":
+    main()
+
